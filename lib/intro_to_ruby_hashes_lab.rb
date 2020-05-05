efc5180d0c6e864c621824839b0aff4ebfe72688
Paul Ralyea <047par@gmail.com>
2020-05-05 19:24:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 
 def read_from_hash(hash, key)
   hash_to_read = {hash=>key}
-  read_from_hash = (hash, key)
+  read_from_hash = (hash, key=)
   # return the correct value using the hash and key parameters
 end
 
