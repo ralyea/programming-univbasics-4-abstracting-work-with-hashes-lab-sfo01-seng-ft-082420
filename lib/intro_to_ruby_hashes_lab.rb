68aaaca550d964f4c60be0be5bf81d22d0eee55e
Paul Ralyea <047par@gmail.com>
2020-05-05 19:23:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash_to_read = {hash=>}
+  hash_to_read = {hash=>key}
   read_from_hash = (hash, key)
   # return the correct value using the hash and key parameters
 end
